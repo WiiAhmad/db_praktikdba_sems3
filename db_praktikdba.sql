8d91ebf0dab65374b70af05e90e3a31daac24919
Ahmad maulana yusuf <77580091+WiiAhmad@users.noreply.github.com>
2023-10-03 20:12:34 +0700

    add insert into

--- db_praktikdba.sql
+++ db_praktikdba.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 24, 2023 at 05:54 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: localhost
+-- Generation Time: Oct 03, 2023 at 08:05 AM
+-- Server version: 5.7.24
+-- PHP Version: 7.2.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,15 +31,30 @@ CREATE TABLE `tb_barang` (
   `kode_barang` varchar(50) NOT NULL,
   `nama_barang` varchar(255) DEFAULT NULL,
   `warna` varchar(100) DEFAULT NULL,
-  `satuan` varchar(50) DEFAULT NULL,
   `kode_kategori` varchar(5) DEFAULT NULL,
+  `satuan` varchar(50) DEFAULT NULL,
   `stok` int(11) DEFAULT NULL,
   `harga_beli` int(11) DEFAULT NULL,
   `harga_jual` int(11) DEFAULT NULL,
   `profit` int(11) DEFAULT NULL,
   `tgl_input` date DEFAULT NULL,
   `pengguna` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_barang`
+--
+
+INSERT INTO `tb_barang` (`kode_barang`, `nama_barang`, `warna`, `kode_kategori`, `satuan`, `stok`, `harga_beli`, `harga_jual`, `profit`, `tgl_input`, `pengguna`) VALUES
+('B001', 'Kemeja Anak', 'Biru', 'K003', 'Pcs', 26, 18000, 27000, 9000, '2022-08-15', 1),
+('B002', 'Blazer Anak', 'Hitam', 'K004', 'Pcs', 13, 17000, 25000, 8000, '2022-08-15', 1),
+('B003', 'Rok Mono', 'Pink', 'K004', 'Pcs', 18, 20000, 26000, 6000, '2022-09-15', 1),
+('B004', 'Kerudung Anak', 'Coklat', 'K004', 'Pcs', 26, 10000, 15000, 5000, '2022-09-15', 1),
+('B005', 'Kerudung Dewasa', 'Coklat', 'K002', 'Pcs', 9, 15000, 20000, 5000, '2022-09-15', 1),
+('B006', 'Kaos Oblong', 'Putih', 'K001', 'Pcs', 11, 16000, 23000, 7000, '2022-09-15', 1),
+('B007', 'Kaos Kerah', 'Merah', 'K001', 'Pcs', 11, 20000, 25000, 5000, '2022-09-15', 1),
+('B008', 'Celana Pendek Anak', 'Biru', 'K003', 'Pcs', 11, 120000, 150000, 30000, '2022-09-15', 1),
+('B009', 'Celana Panjang Jeans', 'Biru', 'K001', 'Pcs', 26, 130000, 250000, 120000, '2022-09-15', 1);
 
 -- --------------------------------------------------------
 
@@ -52,7 +67,20 @@ CREATE TABLE `tb_kategori` (
   `nama_kategori` varchar(50) DEFAULT NULL,
   `tgl_input` date DEFAULT NULL,
   `pengguna` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_kategori`
+--
+
+INSERT INTO `tb_kategori` (`kode_kategori`, `nama_kategori`, `tgl_input`, `pengguna`) VALUES
+('K001', 'Baju Dewasa Laki-laki', '2022-09-15', 1),
+('K002', 'Baju Dewasa Perempuan', '2022-09-15', 1),
+('K003', 'Baju Anak Laki-laki', '2022-09-15', 1),
+('K004', 'Baju Anak Perempuan', '2022-09-15', 1),
+('K005', 'Topi Dewasa', '2022-09-15', 1),
+('K006', 'Produk Kebersihan', '2022-10-07', 1),
+('K007', 'testing', '2023-06-19', 3);
 
 -- --------------------------------------------------------
 
@@ -63,12 +91,21 @@ CREATE TABLE `tb_kategori` (
 CREATE TABLE `tb_pelanggan` (
   `kode_pelanggan` int(11) NOT NULL,
   `nama` varchar(100) DEFAULT NULL,
-  `alamat` text DEFAULT NULL,
+  `alamat` text,
   `telepon` varchar(15) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
   `tgl_input` date DEFAULT NULL,
   `pengguna` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_pelanggan`
+--
+
+INSERT INTO `tb_pelanggan` (`kode_pelanggan`, `nama`, `alamat`, `telepon`, `email`, `tgl_input`, `pengguna`) VALUES
+(1, 'Umum', NULL, NULL, NULL, '2022-09-15', 1),
+(2, 'Budi Raharjo', 'Plumbon Cirebon', '081312121212', NULL, '2022-09-15', 3),
+(3, 'Wati', 'Plered', '084545454545', NULL, '2022-09-15', 3);
 
 -- --------------------------------------------------------
 
@@ -82,7 +119,15 @@ CREATE TABLE `tb_pembelian_detail` (
   `kode_barang` varchar(50) DEFAULT NULL,
   `jumlah` int(11) DEFAULT NULL,
   `pengguna` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_pembelian_detail`
+--
+
+INSERT INTO `tb_pembelian_detail` (`id`, `tgl`, `kode_barang`, `jumlah`, `pengguna`) VALUES
+(6, '2020-12-04', 'B0001', 5, 3),
+(7, '2022-09-07', 'B0001', 20, 3);
 
 -- --------------------------------------------------------
 
@@ -92,13 +137,22 @@ CREATE TABLE `tb_pembelian_detail` (
 
 CREATE TABLE `tb_pengguna` (
   `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
   `username` varchar(100) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
   `level` varchar(20) DEFAULT NULL,
   `foto` varchar(255) DEFAULT NULL,
   `tgl_input` date DEFAULT NULL,
   `pengguna` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_pengguna`
+--
+
+INSERT INTO `tb_pengguna` (`id`, `nama`, `username`, `password`, `level`, `foto`, `tgl_input`, `pengguna`) VALUES
+(1, 'admin', 'Rina Maelani', 'admin', 'admin', 'kartini.jpg', '2022-09-01', 1),
+(3, 'kasir', 'Candra', 'kasir', 'kasir', 'user.jpg', '2022-09-14', 1);
 
 -- --------------------------------------------------------
 
@@ -116,7 +170,16 @@ CREATE TABLE `tb_penjualan` (
   `potongan` int(11) DEFAULT NULL,
   `total_b` int(11) DEFAULT NULL,
   `pengguna` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_penjualan`
+--
+
+INSERT INTO `tb_penjualan` (`kode_penjualan`, `tgl_penjualan`, `id_pelanggan`, `bayar`, `kembali`, `diskon`, `potongan`, `total_b`, `pengguna`) VALUES
+('PJ-00000001', '2021-12-04', 2, 350000, 0, 0, 0, 350000, 3),
+('PJ-00000002', '2021-12-04', 1, 100000, 27000, 0, 0, 73000, 3),
+('PJ-00000003', '2022-09-15', 2, 100000, 27000, 0, 0, 73000, 3);
 
 -- --------------------------------------------------------
 
@@ -130,7 +193,19 @@ CREATE TABLE `tb_penjualan_detail` (
   `kode_bareng` varchar(50) DEFAULT NULL,
   `jumlah` int(11) DEFAULT NULL,
   `total` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_penjualan_detail`
+--
+
+INSERT INTO `tb_penjualan_detail` (`id`, `kode_penjualan`, `kode_bareng`, `jumlah`, `total`) VALUES
+(78, 'PJ-00000001', 'B0001', 2, 50000),
+(79, 'PJ-00000002', 'B0008', 2, 300000),
+(80, 'PJ-00000003', 'B0001', 2, 50000),
+(81, 'PJ-00000004', 'B0002', 1, 23000),
+(82, 'PJ-00000005', 'B0001', 2, 50000),
+(83, 'PJ-00000006', 'B0002', 1, 23000);
 
 --
 -- Indexes for dumped tables
@@ -186,19 +261,19 @@ ALTER TABLE `tb_penjualan_detail`
 -- AUTO_INCREMENT for table `tb_pembelian_detail`
 --
 ALTER TABLE `tb_pembelian_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tb_pengguna`
 --
 ALTER TABLE `tb_pengguna`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tb_penjualan_detail`
 --
 ALTER TABLE `tb_penjualan_detail`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=84;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
